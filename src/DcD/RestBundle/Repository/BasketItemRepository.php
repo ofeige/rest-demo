47f342848db645c87cdf0301c82f27f91851ec5c
Oliver Feige <oliver@feige.net>
2016-11-17 00:03:26 +0100

    fix lazzy loading issues

--- src/DcD/RestBundle/Repository/BasketItemRepository.php
+++ src/DcD/RestBundle/Repository/BasketItemRepository.php
@@ -10,4 +10,47 @@ namespace DcD\RestBundle\Repository;
  */
 class BasketItemRepository extends \Doctrine\ORM\EntityRepository
 {
+    /**
+     * get all basketItems that are belongs to a basket
+     * implemented to avoid lazy loading always the basket entity
+     *
+     * @param $basketId
+     * @return array
+     */
+    public function getItems($basketId)
+    {
+        return $this->getEntityManager()->createQuery('
+            SELECT 
+              bi.id, 
+              bi.info 
+            FROM 
+              RestBundle:BasketItem bi 
+            WHERE 
+              bi.basket = :id
+         ')
+            ->setParameter('id', $basketId)
+            ->getResult();
+    }
+
+    /**
+     * get one basketItem
+     * implemented to avoid lazy loading always the basket entity
+     *
+     * @param $basketItemId
+     * @return array
+     */
+    public function getItem($basketItemId)
+    {
+        return $this->getEntityManager()->createQuery('
+            SELECT 
+              bi.id, 
+              bi.info 
+            FROM 
+              RestBundle:BasketItem bi 
+            WHERE 
+              bi.id = :id
+         ')
+            ->setParameter('id', $basketItemId)
+            ->getResult();
+    }
 }
