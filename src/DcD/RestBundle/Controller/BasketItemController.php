80a5a83e8f17efde6b8ce7827a900b62a278286f
Oliver Feige <oliver@feige.net>
2016-11-17 00:01:11 +0100

    add basketItem Actions

--- src/DcD/RestBundle/Controller/BasketItemController.php
+++ src/DcD/RestBundle/Controller/BasketItemController.php
@@ -2,12 +2,85 @@
 
 namespace DcD\RestBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use DcD\RestBundle\Entity\Basket;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
 
-class BasketItemController extends Controller
+/**
+ */
+class BasketItemController extends FOSRestController implements ClassResourceInterface
 {
-    public function indexAction($name)
+    /**
+     * @Rest\View
+     */
+    public function cgetAction($basketId)
     {
-        return $this->render('', array('name' => $name));
+        $items = $this->getBasketItemRepository()->getItems($id);
+
+        return $items;
+    }
+
+    /**
+     * @Rest\View
+     */
+    public function getAction($basketId, $itemId)
+    {
+        $items = $this->getBasketItemRepository()->getItem($itemId);
+
+        return $items;
+    }
+
+    /**
+     * @Rest\View
+     * @ParamConverter("basket", class="array", converter="fos_rest.request_body")
+     */
+    public function postAction(Request $request, $basketId)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $basket = $em->getRepository('RestBundle:Basket')->find($basketId);
+
+        $basketItem = new \DcD\RestBundle\Entity\BasketItem();
+        $basketItem->setBasket($basket);
+
+        $form = $this->createForm(\DcD\RestBundle\Form\Type\BasketItemType::class, $basketItem );
+        $form->submit($request->get('basket'), false);
+
+        if ( $form->isValid() ) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist( $basketItem );
+            $em->flush();
+
+            //avoid lazzy loading
+            $basketItem->setBasket(NULL);
+            return $this->view($basketItem, \Symfony\Component\HttpFoundation\Response::HTTP_CREATED);
+        }
+
+        return $form;
+    }
+
+    /**
+     * @Rest\View()
+     */
+    public function deleteAction($basketId, $basketItemId)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $basketItem = $em->getRepository('RestBundle:BasketItem')->find($basketItemId);
+
+        $em->remove($basketItem);
+        $em->flush();
+
+        return $this->view(null, \Symfony\Component\HttpFoundation\Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * @return \DcD\RestBundle\Repository\BasketItemRepository
+     */
+    private function getBasketItemRepository()
+    {
+        return $this->getDoctrine()
+            ->getRepository('RestBundle:BasketItem');
     }
 }
